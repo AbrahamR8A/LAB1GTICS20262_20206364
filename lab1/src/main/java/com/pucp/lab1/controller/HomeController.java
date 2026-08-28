534ebcf04cb67fcd4453ff0dceca6202ca3d1c83
AbrahamR8A <a20206364@pucp.edu.pe>
2026-08-27 21:35:53 -0500

    segundo Commit

--- lab1/src/main/java/com/pucp/lab1/controller/HomeController.java
+++ lab1/src/main/java/com/pucp/lab1/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.pucp.lab1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -10,6 +11,13 @@ public class HomeController {
     @GetMapping("/registrar")
     public String registrar(Model model) {
 
+        return "registrar";
     }
+
+    @PostMapping("/guardar")
+    public String guardar(Model model) {
+        return "ListaRegistros";
+    }
+
 
 }
